106835e621c6e29c6c84468d877dbda0ffd9cba8
ShadyElia <shady.elia@hotmail.com>
2019-07-18 22:30:09 +0200

    add web app and view to hello world

--- HelloWorld/urls.py
+++ HelloWorld/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from web.urls import web_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns = [
+    re_path('', include(web_urls))
 ]
